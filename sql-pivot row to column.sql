39b0e7dd42c841f945b36a396093b2ac6a4816a4
Subhasish Das <54557315+sdas2k18-tech@users.noreply.github.com>
2024-08-28 20:06:13 +0530

    Update sql-pivot row to column.sql

--- sql-pivot row to column.sql
+++ sql-pivot row to column.sql
@@ -15,10 +15,48 @@ values (1,'salary',10000),(1,'bonus',5000),(1,'hike_percent',10)
 select * from emp_compensation;
 
 // sum with case would not give the null values while pivoting from row tabele to columen table 
+// cannot into use into keyword directly for making the new table 
+// Database System: Different SQL databases handle INTO differently. For instance:
 
-select emp_id, 
-sum(case when salary_component_type = 'salary' then val end) as salary,
-sum(case when salary_component_type = 'bonus' then val end) as bonus,
-sum(case when salary_component_type = 'hike_percent' then val end) as hike_percent
-from emp_compensation
-group by emp_id
+-- In Microsoft SQL Server and PostgreSQL, INTO is used to create a new table and insert data into it.
+-- In MySQL and SQLite, INTO is not supported in this way for table creation. You would need to use a CREATE TABLE statement before running an INSERT INTO query. 
+
+CREATE or replace TABLE emp_compensation_pivot (
+    emp_id INT,
+    salary DECIMAL(10, 2),
+    bonus DECIMAL(10, 2),
+    hike_percent DECIMAL(5, 2)
+);
+
+INSERT INTO emp_compensation_pivot (emp_id, salary, bonus, hike_percent)
+SELECT emp_id, 
+    SUM(CASE WHEN salary_component_type = 'salary' THEN val ELSE 0 END) AS salary,
+    SUM(CASE WHEN salary_component_type = 'bonus' THEN val ELSE 0 END) AS bonus,
+    SUM(CASE WHEN salary_component_type = 'hike_percent' THEN val ELSE 0 END) AS hike_percent
+FROM emp_compensation
+GROUP BY emp_id;
+
+
+
+-- in case of mysql servers 
+-- select emp_id, 
+-- sum(case when salary_component_type = 'salary' then val end) as salary,
+-- sum(case when salary_component_type = 'bonus' then val end) as bonus,
+-- sum(case when salary_component_type = 'hike_percent' then val end) as hike_percent
+-- into emp_compensation_pivot
+-- from emp_compensation
+-- group by emp_id
+
+select * from emp_compensation_pivot
+
+// unpivoting
+select * from (
+select emp_id, 'salary' as salary_component_type, salary as val from emp_compensation_pivot
+union all
+select emp_id, 'bonus' as salary_component_type, bonus as val from emp_compensation_pivot
+union all
+select emp_id, 'hike_percent' as salary_component_type, hike_percent as val from emp_compensation_pivot
+)
+order by emp_id
+
+
